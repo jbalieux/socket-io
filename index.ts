ed7cc7a153c964b048617f5e85c4979f957ce120
NekouSama <nekousama@senapi.fr>
2019-06-26 23:49:26 +0200

    Remove express for fastify

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 const fastify = require('fastify')({ logger: true });
 var io = require('socket.io')(fastify.server);
 
-fastify.listen(3001);
+fastify.listen(3001, '::');
 
 io.on('connection', function(socket) {
   console.log('a user connected');
